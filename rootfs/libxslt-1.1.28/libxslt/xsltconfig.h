50faa59f47db8aa12d9c87867d98cb9177b558d9
aifi <liujf@autelan.com>
2014-11-25 04:46:12 -0500

    modify libxslt-1.1.28

--- rootfs/libxslt-1.1.28/libxslt/xsltconfig.h
+++ rootfs/libxslt-1.1.28/libxslt/xsltconfig.h
@@ -120,7 +120,7 @@ extern "C" {
 #ifndef WITH_MODULES
 #define WITH_MODULES
 #endif
-#define LIBXSLT_DEFAULT_PLUGINS_PATH() "/home/hisisdk/histb/autelan/release/lib/libxslt-plugins"
+#define LIBXSLT_DEFAULT_PLUGINS_PATH() "/home/hisisdk/histb/autelan/release/usr/lib/libxslt-plugins"
 #endif
 
 /**
