774a548128c023efbc6b0c04bddc89a2fb94499a
aifi <liujf@autelan.com>
2014-11-25 21:42:02 -0500

    modify rootfs

--- rootfs/libxslt-1.1.28_ok/libxslt-1.1.28/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/hisisdk/histb/autelan/release/usr/lib"
-XSLT_LIBS="-lxslt  -lxml2 -lm -lrt"
-XSLT_INCLUDEDIR="-I/home/hisisdk/histb/autelan/fakerelease/usr/include"
-MODULE_VERSION="xslt-1.1.28"
